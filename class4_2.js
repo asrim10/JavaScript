e6584688f69580b8b0220ab44d15abac6ff9e7cb
asrim10 <asrimmsuwall@gmail.com>
2025-11-07 10:14:51 +0545

    sequential_and_parallel_promise

--- class4_2.js
+++ class4_2.js
@@ -10,8 +10,8 @@ const delay = (ms) => {
   return new Promise((resolve, reject) => {
     // time consuming function
     setTimeout(() => {
-      //   resolve(`Success`); // success statemet/callback
-      reject(`Failure`); // failure statement
+      resolve(`Success`); // success statemet/callback
+      //   reject(`Failure`); // failure statement
     }, ms);
   });
 };
@@ -31,3 +31,19 @@ const promiseCall = async () => {
   console.log("End");
 };
 promiseCall();
+
+const sequentialPromise = async () => {
+  console.time("sequentialPromise");
+  const res1 = await delay(3000);
+  console.log(res1);
+  const res2 = await delay(5000);
+  console.log(res2);
+  //wait time is sum of both
+  console.timeEnd("sequentialPromise");
+};
+
+const parallelPromise = async () => {
+  const [res1, res2] = await Promise.all([delay(3000), delay(5000)]); // wait time is max of both
+  console.log(res1, res2);
+  console.timeEnd("parallelPromise");
+};

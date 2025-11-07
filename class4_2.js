80448fde436551a9e0198956adeb9d104b2a48c1
asrim10 <asrimmsuwall@gmail.com>
2025-11-07 09:45:18 +0545

    class

--- class4_2.js
+++ class4_2.js
@@ -11,7 +11,7 @@ const delay = ms => {
         (resolve, reject) => {
             setTimeout(() => {
                 resolve(`Success`) // success statemet/callback
-            },ms)
+            }, ms)
         }
     )
 }

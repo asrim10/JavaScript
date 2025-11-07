7f85cc4a26224c6f597f61a479f7f543f48be603
asrim10 <asrimmsuwall@gmail.com>
2025-11-07 10:40:39 +0545

    class4_task

--- class4_task.js
+++ class4_task.js
@@ -8,3 +8,25 @@
 // make a new function
 // log the resolved and rejected values separately
 // using non - blocking . then, run 1000, 2000, 3000, sequentially
+
+const delayWithNum = (ms, num) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      num % 2 == 0 ? resolve(`Resolved: ${num}`) : reject(`Rejected: ${num}`);
+    }, ms);
+  });
+};
+delayWithNum(2000, 3)
+  .then((res) => {
+    delayWithNum(1000, 4)
+      .then((res2) => {
+        console.log(res2);
+        delayWithNum(3000, 5)
+          .then((res3) => console.log(res3))
+          .catch((err3) => console.log(err3));
+      })
+      .catch((err2) => console.log(err2));
+  })
+  .catch((err) => {
+    console.log(err);
+  });

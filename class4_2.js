2c5f07a1fb4a396d3b1e9add703e96782cfb3c32
asrim10 <asrimmsuwall@gmail.com>
2025-11-07 10:06:33 +0545

    class

--- class4_2.js
+++ class4_2.js
@@ -1,20 +1,33 @@
 // Promise async/await
 // Future then/catch
 
-console.log(1)
-// setTimeout(() => console.log(2), 1000) // run after 1 sec
-console.log(3) 
+console.log(1);
+// setTimeout(() => console.log(2), 1000); // run after 1 sec
+console.log(3);
 
 //Promise
-const delay = ms => {
-    return new Promise(
-        (resolve, reject) => {
-            setTimeout(() => {
-                resolve(`Success`) // success statemet/callback
-            }, ms)
-        }
-    )
-}
-console.log("Start")
-delay(2000).then((msg => console.log(msg))) // wait for promise to resolve
-console.log("End")
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    // time consuming function
+    setTimeout(() => {
+      //   resolve(`Success`); // success statemet/callback
+      reject(`Failure`); // failure statement
+    }, ms);
+  });
+};
+
+const promiseCall = async () => {
+  console.log("Start");
+  try {
+    const res = await delay(2000); //blocking
+    console.log(res); //resolved value
+  } catch (err) {
+    console.log(err); // rejected vale
+  }
+  delay(2000) // non blocking
+    .then((msg) => console.log(msg)) // wait for promise to resolve
+    .catch((err) => console.log(err)); // wait for promise to reject
+
+  console.log("End");
+};
+promiseCall();
